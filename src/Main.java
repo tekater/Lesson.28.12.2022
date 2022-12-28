60fc4e52c91eacee666bb0fe9a5ba55501bc9c9a
tekater <120014627+tekater@users.noreply.github.com>
2022-12-28 19:40:04 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введите цифру дважды");
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int max = a > b ? a : b;
+        // Первый случай использования тернарного оператора - Start
+        int max = a > b ? a : b; // тернарный оператор
 
         /* if (a > b) {
             max = a;
         } else {
             max = b;
         }*/
+
+        // Первый случай использования тернарного оператора - end
+        //
+        // Второй случай использования тернарного оператора - start
+        String result = a == b ? "equal" :
+                a > b ? "more" : "less";
+        System.out.println(result);
+        if ( a == b) {
+            result = "равно";
+        } else if (a > b) {
+            result = "больше";
+        }else {
+            result = "меньше";
+        } System.out.println(result);
+        // Второй случай использования тернарного оператора - end
         System.out.println(max);
+        // Тернарный оператор - start
+        //                  if (10 > 9)              "выражение верно" "выражение не верно"
+        //String result = выражение сравнения ? если верно выражние : сли выражение не верно;
+        // Тернарный оператор - end
     }
 }
